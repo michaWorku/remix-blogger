cebcda3cfebe073f56ede7543ba9e9eeac08b84c
michaWorku <mikias27worku@gmail.com>
2022-06-06 15:32:19 +0300

    app created with document and layout

---
+++ app/root.jsx
@@ -0,0 +1,60 @@
+import { Link, Links, LiveReload, Meta, Outlet } from "@remix-run/react";
+import globalStylesUrl from './styles/global.css'
+
+export const links = () =>[{rel: 'stylesheet', href: globalStylesUrl}]
+
+export const meta = ()=>{
+  const description = 'A cool blog built with Remix'
+  const keywords = 'remix, react, javascript'
+
+  return {
+    description,
+    keywords
+  }
+}
+
+export default function App(){
+  return (
+   <Document>
+     <Layout>
+      <Outlet/>
+     </Layout>
+   </Document>
+  )
+}
+
+function Document({children, title}){
+  return (
+    <html lang="en">
+      <head>
+        <Meta/>
+        <Links/>
+        <title>{title ? title: "Remix blog"}</title>
+      </head>
+      <body>
+        {children}
+        {
+          process.env.NODE_ENV === 'development' ? <LiveReload/> : null
+        }
+      </body>
+    </html>
+  )
+}
+
+function Layout({children}) {
+  return(
+    <>
+      <nav className="navbar">
+        <Link to='/' className='logo'>Remix</Link>
+        <ul>
+          <li>
+          <Link to='/posts' className='nav'>Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">
+        {children}
+      </div>
+    </>
+  )
+}
