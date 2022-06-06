1a423e645277f5b82c98ba06693a23aa96a3c3d4
michaWorku <mikias27worku@gmail.com>
2022-06-06 17:51:47 +0300

    load single post done

--- app/routes/posts/$postId.jsx
+++ app/routes/posts/$postId.jsx
@@ -1,10 +1,40 @@
-import { useParams } from "@remix-run/react"
+import { Link, useLoaderData } from "@remix-run/react"
+import { db } from '~/utils/db.server'
+
+export const loader = async ({params})=>{
+  
+  const post = await db.post.findUnique({
+    where: {id: params.postId}
+  })
+
+  if(!post) throw new Error('Post not found')
+
+  const data = {post}
+
+  return data
+}
 
 function Post() {
-  const {postId} = useParams()
+  const { post } = useLoaderData()
   return (
     <div>
-        <h1>Post {postId}</h1>
+      <div className='page-header'>
+        <h1>{post.title}</h1>
+        <Link to='/posts' className='btn btn-reverse'>
+          Back
+        </Link>
+      </div>
+
+      <div className='page-content'>{post.body}</div>
+
+      {/* <div className='page-footer'>
+        {user.id === post.userId && (
+          <form method='POST'>
+            <input type='hidden' name='_method' value='delete' />
+            <button className='btn btn-delete'>Delete</button>
+          </form>
+        )}
+      </div> */}
     </div>
   )
 }
