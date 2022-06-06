85f9eab2002f4a894548ca5b013823a7a31cc4a5
michaWorku <mikias27worku@gmail.com>
2022-06-06 16:11:47 +0300

    action for posting created

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,9 +1,42 @@
+import { Link } from "@remix-run/react"
+import { redirect } from "@remix-run/node"
+
+export const action = async ({request}) =>{
+    
+    const form = await request.formData()
+    const title = form .get('title')
+    const body = form.get('body')
+
+    const fields = {title, body}
+
+    // @todo - submit to database
+    console.log(fields)
+
+    return redirect('/posts')
+}
 
 function NewPost() {
   return (
-    <div>
+   <>
+    <div className="page-header">
         <h1>new posts</h1>
+        <Link to='/posts' className='btn btn-reverse'>Back</Link>
     </div>
+    <div className="page-content">
+        <form method="POST">
+            <div className="form-control">
+                <label htmlFor="title">Title</label>
+                <input type="text" name='title' id='title' />
+            </div>
+            <div className="form-control">
+                <label htmlFor="body">Post Body</label>
+                <textarea name='body' id='body' />
+            </div>
+            
+            <button type="submit" className="btn btn-block">Add Post</button>
+        </form>
+    </div>
+   </>
   )
 }
 
