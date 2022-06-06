bf29052655f43db5feedc715dc583eb52b599fb8
michaWorku <mikias27worku@gmail.com>
2022-06-06 15:34:00 +0300

    post route created

---
+++ app/routes/posts.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "@remix-run/react"
+
+function Posts(){
+    return(
+        <>
+         <Outlet/>
+        </>
+    )
+}
+
+export default Posts
---
+++ app/routes/posts/index.jsx
@@ -0,0 +1,10 @@
+
+function Post() {
+  return (
+    <div>
+        <h1>posts</h1>
+    </div>
+  )
+}
+
+export default Post
