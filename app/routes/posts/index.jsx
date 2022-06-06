95b3ef4334ffb0e53a4954b28ab0ccaf854079c9
michaWorku <mikias27worku@gmail.com>
2022-06-06 15:50:21 +0300

    make the post route loader

--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,9 +1,36 @@
+import { Link, useLoaderData } from "@remix-run/react"
+
+export const loader = () =>{
+  const data = {
+    posts: [
+      {id: 1, title: 'Post 1', body: 'This is a test post'},
+      {id: 2, title: 'Post 2', body: 'This is a test post'},
+      {id: 3, title: 'Post 3', body: 'This is a test post'}
+    ]
+  }
+  return data
+}
 
 function PostItems() {
+  const {posts} = useLoaderData()
   return (
-    <div>
-        <h1>Posts</h1>
-    </div>
+    <>
+        <div className="page-header">
+          <h1>Posts</h1>
+          <Link to='/posts/new'>New Post</Link>
+        </div>
+        <ul className="posts-list">
+          {
+            posts.map((post)=>(
+            <li key={post.id}>
+              <Link to={post.id}>
+                <h3>{post.title}</h3>
+              </Link>
+            </li>)
+            )
+          }
+        </ul>
+    </>
   )
 }
 
