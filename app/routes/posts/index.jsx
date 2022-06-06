b70bbb486062517e68e16a7d716d470dddf01e8f
michaWorku <mikias27worku@gmail.com>
2022-06-06 17:38:42 +0300

    load posts via loader done

--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,12 +1,13 @@
 import { Link, useLoaderData } from "@remix-run/react"
+import {db} from '../../utils/db.server'
 
-export const loader = () =>{
-  const data = {
-    posts: [
-      {id: 1, title: 'Post 1', body: 'This is a test post'},
-      {id: 2, title: 'Post 2', body: 'This is a test post'},
-      {id: 3, title: 'Post 3', body: 'This is a test post'}
-    ]
+export const loader = async() =>{
+  const data =  {
+    posts: await db.post.findMany({
+      take: 20,
+      select: {id:true, title: true, createdAt: true},
+      orderBy: {createdAt: 'desc'}
+    })
   }
   return data
 }
@@ -25,6 +26,9 @@ function PostItems() {
             <li key={post.id}>
               <Link to={post.id}>
                 <h3>{post.title}</h3>
+                {
+                  new Date(post.createdAt).toLocaleString()
+                }
               </Link>
             </li>)
             )
